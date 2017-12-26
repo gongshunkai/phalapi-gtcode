79483f69d9f697640298e4b9e423e9fe69e359d0
gongshunkai <49078111@qq.com>
2017-12-26 21:19:37 +0800

    phalapi-gtcode

--- src/Lite.php
+++ src/Lite.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace PhalApi\GTCode;
 
 use PhalApi\Exception\BadRequestException;
@@ -26,7 +27,8 @@ class Lite
      *
      * @param  $config array 配置文件
      */
-    public function __construct($config = null) {
+    public function __construct($config = null)
+    {
 
         if ($config === NULL) {
             //获得配置项
@@ -45,7 +47,7 @@ class Lite
         } else {
             throw new BadRequestException('privateKey There is no', 1);
         }
-     
+
         //初始化SDK
         $this->GtLib = new GeetestLib($this->captchaId, $this->privateKey);
     }
@@ -56,11 +58,8 @@ class Lite
      * @param array $data
      * @return mixed
      */
-    public function startCaptchaServlet($param, $new_captcha=1) {
-
-        if(!isset($_SESSION)){
-            session_start();
-        }
+    public function startCaptchaServlet($param, $new_captcha = 1)
+    {
 
         $data = array(
             'user_id' => 'test', # 网站用户id
@@ -68,15 +67,17 @@ class Lite
             'ip_address' => '127.0.0.1' # 请在此处传输用户请求验证时所携带的IP
         );
 
-        $data = array_merge($data,$param);
+        $data = array_merge($data, $param);
 
         //调用预处理接口
         $status = $this->GtLib->pre_process($data, $new_captcha);
 
-        $_SESSION['gtserver'] = $status;
-
         //返回验证字符串接口
-        return $this->GtLib->get_response();
+        $ret = $this->GtLib->get_response();
+
+        $tmp = md5(md5($ret['challenge']) . md5(md5($ret['challenge'])) . $status);
+        $ret['kt'] =  $tmp; //. (string) $status));
+        return $ret;
     }
 
 
@@ -89,31 +90,35 @@ class Lite
      * @param array $param
      * @return int
      */
-    public function verifyLoginServlet($challenge, $validate, $seccode,$param, $json_format=1) {
+    public function verifyLoginServlet($challenge, $validate, $seccode, $kt, $param, $json_format = 1)
+    {
 
-        if(!isset($_SESSION)){
-            session_start();
-        }
-        
         $data = array(
             'user_id' => 'test', # 网站用户id
             'client_type' => 'web', #web:电脑上的浏览器；h5:手机上的浏览器，包括移动应用内完全内置的web_view；native：通过原生SDK植入APP应用的方式
             'ip_address' => '127.0.0.1' # 请在此处传输用户请求验证时所携带的IP
         );
 
-        $data = array_merge($data,$param);
+        $data = array_merge($data, $param);
 
-        if ($_SESSION['gtserver'] == 1) {   //服务器正常
+        $tmp = substr($challenge, 0, 32);
+        $tmp = md5($tmp) . md5(md5($tmp));
+
+        if ($kt !== md5($tmp . 1) && $kt !== md5($tmp . 0)) return 0;
+
+        $kt = ($kt == md5($tmp . 1)) ? 1 : 0;
+
+        if ($kt == 1) {   //服务器正常
             $result = $this->GtLib->success_validate($challenge, $validate, $seccode, $data, $json_format);
             if ($result) {
                 return 1;
-            } else{
+            } else {
                 return 0;
             }
-        }else{  //服务器宕机,走failback模式
+        } else {  //服务器宕机,走failback模式
             if ($this->GtLib->fail_validate($challenge, $validate, $seccode)) {
                 return 1;
-            }else{
+            } else {
                 return 0;
             }
         }
@@ -122,10 +127,9 @@ class Lite
     /**
      * 数组对象取值相关 - 避免出错
      */
-    private function getIndex($arr, $key, $default = '') {
+    private function getIndex($arr, $key, $default = '')
+    {
 
         return isset($arr[$key]) ? $arr[$key] : $default;
     }
-
 }
-
